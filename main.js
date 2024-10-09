6ab675d8b99e57ee485f2a841652cf51693b0d30
danielaconceicao <daniela.3597@gmail.com>
2024-10-09 17:07:12 +0200

    organizing the code

---
+++ main.js
@@ -0,0 +1,10 @@
+const inputNameEl = document.querySelector('#inputName').value;
+const inputNumberEl = document.querySelector('#inputNumber').value;
+const selectEl = document.querySelector('#select');
+
+const btnGenerateEl = document.querySelector('.btn-genera');
+const btnCancelEl = document.querySelector('.btn-annulla');
+
+btnGenerateEl.addEventListener('submit', function(){
+    
+});
