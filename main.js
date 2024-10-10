675b92845b66514aef2206063dfd2c52e47971f6
danielaconceicao <daniela.3597@gmail.com>
2024-10-10 09:15:05 +0200

    validating the inputs

--- main.js
+++ main.js
@@ -10,17 +10,38 @@ formEl.addEventListener('submit', function (e) {
     const inputNumberEl = e.target.inputNumber.value;
     const optionEl = selectEl.options[select.selectedIndex].text;
 
-    const info = `<span>${inputNameEl}</span>
-            <span>${inputNumberEl}</span>
-            <span>${optionEl}</span>
-            <span>${getTicketPrice(inputNumberEl, optionEl)}</span>`;
+    const info = `<h1 class'pb-5'>Il tuo biglietto</h1>
+            <div class='d-flex justify-content-around'>
+                <div>
+                    <h2 class='pe-4'>Nome</h2>
+                    <span>${inputNameEl}</span>
+                </div>
+                <div>
+                    <h2 class='pe-4'>Km da percorrere</h2>
+                    <span>${inputNumberEl}km</span>
+                </div>
+                <div>
+                    <h2 class='pe-4'>Fascia d'età</h2>
+                    <span>${optionEl}</span>
+                </div>
+                <div>
+                    <h2 class='pe-4'>Costo Biglietto</h2>
+                    <span>${getTicketPrice(inputNumberEl, optionEl)}</span>
+                </div>
+            </div>`;
+
+    /* checkingInputValue(inputNameEl, inputNumberEl, optionEl); */
+    if (inputNameEl.length < 0 || inputNumberEl < 20 || optionEl.value === 0) {
+        alert('compilare correttamente tutti i campi');
+        clearInput();
+        return;
+    }
 
     ticketEl.insertAdjacentHTML('beforeend', info);
-
     clearInput();
 });
 
-formEl.addEventListener('reset', function(){
+formEl.addEventListener('reset', function () {
     cancelTicketPurchase();
 });
 
@@ -41,7 +62,7 @@ function getTicketPrice(km, option) {
     //va applicato uno sconto del 40% per gli over 65.
     let discountForSeniors = (40 / 100) * ticketPrice;
     discountForSeniors = ticketPrice - discountForSeniors;
-    let discountForSeniorsFormatted = discountForSeniors.toLocaleString('it-IT', { style: "currency", currency: 'EUR' })
+    let discountForSeniorsFormatted = discountForSeniors.toLocaleString('it-IT', { style: "currency", currency: 'EUR' });
 
 
     if (option === 'Minorenne') {
@@ -51,7 +72,6 @@ function getTicketPrice(km, option) {
     } else {
         return `${formattedTicketPrice}`;
     }
-
 }
 
 function clearInput() {
@@ -59,6 +79,13 @@ function clearInput() {
     document.getElementById('inputNumber').value = '';
 }
 
-function cancelTicketPurchase(){
+function cancelTicketPurchase() {
     document.querySelector('.see-ticket').innerHTML = '';
+}
+
+function checkingInputValue(nome, number) {
+    if (nome.length < 5 || number < 20) {
+        alert('compilare correttamente tutti i campi');
+        return;
+    }
 }
