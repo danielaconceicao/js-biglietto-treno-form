7c483fa94e11a263688ace3a54ec120a2782b878
danielaconceicao <daniela.3597@gmail.com>
2024-10-12 12:39:26 +0200

    modified code

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const selectEl = document.querySelector('#select');
 const formEl = document.querySelector('form');
-const ticketEl = document.querySelector('.see-ticket');
-const totalPrice = document.querySelector('total-price');
+const ticketEl = document.querySelector('.space-ticket');
 
 formEl.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const inputNameEl = e.target.inputName.value;
-    const inputKmEl = e.target.inputNumber.value;
+    const inputKmEl = Number(e.target.inputNumber.value);
     const optionAgeEl = selectEl.options[select.selectedIndex].text;
 
-    const info = genereteTicketMarkup(inputNameEl, inputKmEl, optionAgeEl, getTicketPrice);
+    const markup = genereteTicketMarkup(inputNameEl, inputKmEl, optionAgeEl, calculatesTicketPrice);
 
     /* checkingInputValue(inputNameEl, inputNumberEl, optionEl); */
     if (inputNameEl.length < 0 || inputKmEl < 20 || optionAgeEl.value === 0) {
@@ -19,41 +18,51 @@ formEl.addEventListener('submit', function (e) {
         return;
     }
 
-    ticketEl.insertAdjacentHTML('beforeend', info);
+    //joga na pagina um de cada vez
+    ticketEl.innerHTML = markup;
+    //joga na pagina mais de um
+    //ticketEl.innerHTML += markup;
+    //ticketEl.insertAdjacentHTML('afterbegin', markup);
     clearInput();
 
 });
 
 formEl.addEventListener('reset', cancelTicketPurchase);
 
-function getTicketPrice(km, option) {
-
+/**
+ * 
+ * @param {number} km
+ * @param {string} option 
+ * 
+ *  */ 
+function calculatesTicketPrice(km, option) {
     //il prezzo del biglietto è definito in base ai km (0.21 € al km)
     let pricePerKilometre = 0.21;
-
+    let discount;
+    
     //prezzo normale senza sconti
     let ticketPrice = km * pricePerKilometre;
     let formattedTicketPrice = ticketPrice.toLocaleString('it-IT', { style: "currency", currency: 'EUR' });
 
-    //va applicato uno sconto del 20% per i minorenni
-    let discountForMinors = (20 / 100) * ticketPrice;
-    discountForMinors = ticketPrice - discountForMinors;
-    let discountForMinorsFormatted = discountForMinors.toLocaleString('it-IT', { style: "currency", currency: 'EUR' });
-
-    //va applicato uno sconto del 40% per gli over 65.
-    let discountForSeniors = (40 / 100) * ticketPrice;
-    discountForSeniors = ticketPrice - discountForSeniors;
-    let discountForSeniorsFormatted = discountForSeniors.toLocaleString('it-IT', { style: "currency", currency: 'EUR' });
-
-
     if (option === 'Minor') {
-        return `${discountForMinorsFormatted}`;
+        //va applicato uno sconto del 20% per i minorenni
+        discount = 0.2;
+        ticketPrice -= ticketPrice * discount
+        formattedTicketPrice = ticketPrice.toLocaleString('it-IT', { style: "currency", currency: 'EUR' });
+        return formattedTicketPrice;
+    
     } else if (option === 'Senior') {
-        return `${discountForSeniorsFormatted}`;
-    } else {
-        return `${formattedTicketPrice}`;
+        //va applicato uno sconto del 40% per gli over 65.
+        discount = 0.4;
+        ticketPrice -= ticketPrice * discount
+        formattedTicketPrice = ticketPrice.toLocaleString('it-IT', { style: "currency", currency: 'EUR' });
+        return formattedTicketPrice;
+    
+    }else{
+        return ticketPrice, formattedTicketPrice
     }
 }
+
 
 function clearInput() {
     document.getElementById('inputName').value = '';
@@ -64,17 +73,31 @@ function cancelTicketPurchase() {
     document.querySelector('.see-ticket').innerHTML = '';
 }
 
+/**
+ * 
+ * @param {string} nome 
+ * @param {number} km 
+ *  */  
 function checkingInputValue(nome, km) {
     if (nome.length < 5 || km < 20) {
         alert('compilare correttamente tutti i campi');
         return;
     }
 }
-function genereteTicketMarkup(fullName, km, age, getTicketprice) {
+
+/**
+ * 
+ * @param {number} age 
+ * @param {string} fullName 
+ * @param {function} calculatesTicketPrice
+ * @param {number} km 
+ * 
+ *  */  
+function genereteTicketMarkup(fullName, km, age, calculatesTicketPrice) {
     return `<h1 class'pb-5 text-center'>Il tuo biglietto</h1>
             <div class='d-flex justify-content-around'>
                 <div>
-                    <h2 class='pe-4 h5'>Nome</h2>
+                    <h2 class='pe-4 h5'>Nome Cognome</h2>
                     <span>${fullName}</span>
                 </div>
                 <div>
@@ -87,7 +110,7 @@ function genereteTicketMarkup(fullName, km, age, getTicketprice) {
                 </div>
                 <div>
                     <h2 class='pe-4 h5'>Costo Biglietto</h2>
-                    <span>${getTicketprice(km, age)}</span>
+                    <span>${calculatesTicketPrice(km, age)}</span>
                 </div>
             </div>`
 }
